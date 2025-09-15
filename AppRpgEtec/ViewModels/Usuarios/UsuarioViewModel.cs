3a5ccdd64c560f4ae573727c0de8e277f3e72715
Luiza2112 <ritafilhotes4@gmail.com>
2025-09-15 08:54:10 -0300

    :sparkles: Feat: Terminar de configurar os Pins do usuário para aparecer no mapa.

--- AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
+++ AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -55,6 +55,9 @@ namespace AppRpgEtec.ViewModels.Usuarios
         #endregion
 
         #region Metodos
+
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
                
         public async Task AutenticarUsuario()
         {
@@ -73,6 +76,20 @@ namespace AppRpgEtec.ViewModels.Usuarios
                     Preferences.Set("UsuarioId", uAutenticado.Id);
                     Preferences.Set("UsuarioUsername", uAutenticado.Username);
                     Preferences.Set("UsuarioPerfil", uAutenticado.Perfil);
+
+                    //Início da coleta de Geolocalização atual para atualização da API
+                    _isCheckingLocation = true;
+                    _cancelTokenSource = new CancellationTokenSource();
+                    GeolocationRequest request = 
+                        new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                    Location location = await Geolocation
+                        .Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                    Usuario uLoc = new Usuario();
+                    uLoc.Id = uAutenticado.Id;
+                    uLoc.Latitude = location.Latitude;
+                    uLoc.Longitude = location.Longitude;
 
                     await Application.Current.MainPage
                         .DisplayAlert("Informação", mensagem, "Ok");
